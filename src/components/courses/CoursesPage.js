473dcc7702339880ad4a3b5ac29521007ba2ab22
jeantroiani <digitalguest@gmail.com>
2016-08-10 10:52:20 +0100

    added store, actions and dispatcher

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,10 +1,54 @@
 import React, {Component, PropTypes} from 'react';
+import { connect,  } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            course: {
+                title: ''
+            }
+        }
+
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+    }
+    onTitleChange(e) {
+        const course = this.state.course;
+        course.title = e.target.value;
+        this.setState({course: course});
+    }
+
+    onClickSave(e) {
+        e.preventDefault();
+        //this.props.dispatch(courseActions.createCourse(this.state.course)); //if you didn't connect mapDispatchToProps
+        this.props.actions.createCourse(this.state.course);
+    }
+
+    courseRow(course, index) {
+        return <div key={index}>{course.title}</div>;
+    }
+
     render() {
         return (
             <div>
                <h1>Courses</h1>
+                {this.props.courses.map(this.courseRow)}
+                <h2>Add a course</h2>
+                <form>
+                    <input
+                        type="text"
+                        onChange={this.onTitleChange}
+                        value={this.state.course.title}
+                    />
+                    <input
+                        type="submit"
+                        value="Save"
+                        onClick={this.onClickSave}
+                    />
+                </form>
             </div>
         );
     }
@@ -14,4 +58,20 @@ CoursesPage.propTypes = {
     children: PropTypes.object.isRequired
 };
 
-export default CoursesPage;
+function mapStateToProps(state) {
+    return {
+        courses: state.courses
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+//        createCourse: (course) => {
+//            dispatch(courseActions.createCourse(course));
+//        }
+        actions: bindActionCreators(courseActions, dispatch)
+    }
+}
+
+const connectedStateAndProps = connect(mapStateToProps, mapDispatchToProps);
+export default connectedStateAndProps(CoursesPage);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/Store';
+import {Provider} from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 //import './styles/styles.css';
 
-
+const store = configureStore();  //passing a paramenter here will override the store initial state, useful for SSR
 render(
-    <Router history={browserHistory} routes={routes} />, document.getElementById('app')
+    <Provider store={store}>
+        <Router history={browserHistory} routes={routes} />
+    </Provider>,
+    document.getElementById('app')
 );
